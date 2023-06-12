ba2b83071aaf9a1562674b00b8c7e07818df963b
Perugu Soujanya <122893597+PeruguSoujanya13@users.noreply.github.com>
2023-06-12 22:09:47 +0530

    Add files via upload

---
+++ Spring1/SpringSetup/src/main/java/com/ecommerce/SpringSetupApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSetupApplication {
+
+	public static void main(String[] args, Object SpringApplication) {
+		((Object) SpringApplication).run(SpringSetupApplication.class, args);
+	}
+
+}
+
+//Ctrl + Shift + O
